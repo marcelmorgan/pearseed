85a51917e5fca997d2ed163068660321ccce7ba2
Marcel Morgan <marcel.morgan@gmail.com>
2011-09-03 21:16:07 -0500

    Initial import

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'retort'
+
+Retort::Service.configure do |c|
+ c.url = "http://elijah/RPC2"
+end
+
+get '/' do
+	redirect to('/torrents')
+end
+
+post '/create' do
+	Retort::Torrent.load(params[:url])
+	redirect to('/')
+end
+
+get '/torrents' do
+	redirect to('/torrents/main')
+end
+
+get '/torrents/:view' do
+	@torrents = Retort::Torrent.all params[:view]
+	erb :index
+end
+
+get '/torrents/:id/:action' do
+	t = Retort::Torrent.action params[:action], params[:id]
+	redirect to('/')
+end
