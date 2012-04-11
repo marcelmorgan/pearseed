c72bf2384caf676378f30aef3d25fa46a573f85b
Marcel Morgan <marcel.morgan@gmail.com>
2012-04-10 20:50:10 -0500

    Add unicorn config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,26 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
+
+set :application, "pearseed"
+set :repository, "git@github.com:mcmorgan/#{application}.git"
+set :branch, "master"
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "Apache"                          # Your HTTP server, Apache/etc
-role :app, "Apache"                          # This may be the same as your `Web` server
-#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+role :web, "192.168.1.2"                          # Your HTTP server, Apache/etc
+role :app, "192.168.1.2"                          # This may be the same as your `Web` server
+
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+#set :bundle_flags, "--deployment --quiet --binstubs --shebang ruby-local-exec"
+set :default_environment, {
+    'PATH' => "/home/#{user}/.rbenv/shims:/home/#{user}/.rbenv/bin:$PATH"
+}
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
