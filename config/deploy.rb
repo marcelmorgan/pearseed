4c6ba592beb8cfca3fed739e9bf4822aa9c53442
Marcel Morgan <marcel.morgan@codedry.com>
2012-11-15 15:50:50 -0500

    Update capistrano deployment for Rasberry Pi

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,33 @@
 require "bundler/capistrano"
 
+load "config/recipes/base"
+# load "config/recipes/rbenv"
+
 set :application, "pearseed"
 set :repository, "git@github.com:mcmorgan/#{application}.git"
 set :branch, "master"
 
 set :scm, :git
 
-role :web, "192.168.1.2"                          # Your HTTP server, Apache/etc
-role :app, "192.168.1.2"                          # This may be the same as your `Web` server
+role :web, "192.168.1.132"                          # Your HTTP server, Apache/etc
+role :app, "192.168.1.132"                          # This may be the same as your `Web` server
 
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
-#set :bundle_flags, "--deployment --quiet --binstubs --shebang ruby-local-exec"
-set :default_environment, {
-    'PATH' => "/home/#{user}/.rbenv/shims:/home/#{user}/.rbenv/bin:$PATH"
-}
+
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+namespace :bundler do
+  desc "Install bundler"
+  task :install, roles: :app do
+    run "gem install bundler --no-ri --no-rdoc"
+  end
+  after "deploy:setup", "bundler:install"
+end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+
